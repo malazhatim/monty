c7ec05da99be6fb2781688b4f481388b809d0c93
Esra-333 <esra.omer444@gmail.com>
2023-12-14 04:01:01 -0800

    a

--- pop_monty.c
+++ pop_monty.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 /**
  * pop - add node to stack
- * @head: pointer
+ * @stack: pointer
  * @l_n: integer
  */
 void pop(stack_t **stack, unsigned int l_n)
@@ -12,7 +12,7 @@ void pop(stack_t **stack, unsigned int l_n)
 		mon_errors(7, l_n);
 	tmp = *stack;
 	*stack = tmp->next;
-	if(*stack != NULL)
+	if (*stack != NULL)
 		(*stack)->prev = NULL;
 	free(tmp);
 }
